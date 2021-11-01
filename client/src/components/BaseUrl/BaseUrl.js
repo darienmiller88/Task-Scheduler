65e7a28064600797a22dcb00b44c3667166b2e55
darienmiller88 <darienmiller88@yahoo.com>
2021-11-01 02:39:11 -0400

    changed api url

--- client/src/components/BaseUrl/BaseUrl.js
+++ client/src/components/BaseUrl/BaseUrl.js
@@ -1,5 +1,5 @@
 import Port from "../Port/Port"
 
-const base =  window.location.hostname === "localhost" ? `http://localhost:${Port}/api/v1/reminders/` : `https://thetaskscheduler.herokuapp.com/api/v1/reminders/`
+const base =  window.location.hostname === "localhost" ? `http://localhost:${Port}/api/v1/reminders/` : `https://thetaskschedulerapi.herokuapp.com/api/v1/reminders/`
 
 export default base 
